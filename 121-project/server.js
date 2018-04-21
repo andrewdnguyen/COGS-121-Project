df7a55fadfa04c26ba98585d19d61328b241b7fa
Andrew Nguyen <andrewdnguyen64@gmail.com>
2018-04-21 16:32:48 -0700

    VERY basic server

---
+++ 121-project/server.js
@@ -0,0 +1,23 @@
+// Prerequisites - first run:
+//   npm install
+//
+// which will look in package.json and install all dependencies
+// (e.g., express)
+//
+// To start the server, run:
+//   node server.js
+//
+// and open the frontend webpage at http://localhost:3000/
+
+const express = require('express');
+const app = express();
+
+const index = require('./static_files/index.html');
+
+// put all of your static files (e.g., HTML, CSS, JS, JPG) in the static_files/
+
+app.use(express.static('static_files'));
+
+app.listen(3000, () => {
+  console.log('Server started at http://localhost:3000/index.html');
+});
